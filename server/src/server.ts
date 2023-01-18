3b998fc9c6187ea2015e920c53ee2b806e906ff2
Cristopher Paiva <josuepaiva7@gmail.com>
2023-01-18 04:32:09 -0300

    chore: prisma configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,18 @@
 import Fastify from "fastify"
+import { PrismaClient} from "@prisma/client"
+
 
 const app = Fastify()
+const prisma = new PrismaClient()
 
-app.get('/', ()=>{
-  return "hello world"
+
+app.get('/', async ()=>{
+  const habist = await prisma.habit.findMany()
+  return habist
 })
 
 app.listen({
   port: 3333,
+}).then(() => {
+  console.log("HTTP Server running!");
 })
